e80a449e636fd3984bde2ca334f23acd6729f546
Shubhamkurunde <109518432+Shubhamkurunde@users.noreply.github.com>
2022-10-17 13:56:47 +0530

    service component and interface created

---
+++ src/app/component/popular-movies/popular-movies.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/Services/data.service';
+import { StateService } from 'src/app/Services/state.service';
+
+@Component({
+  selector: 'app-popular-movies',
+  templateUrl: './popular-movies.component.html',
+  styleUrls: ['./popular-movies.component.css']
+})
+export class PopularMoviesComponent implements OnInit {
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  }
+
+ 
+
+}
